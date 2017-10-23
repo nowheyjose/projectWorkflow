23846a95e54b517e6885a5f706183bab2a247a3b
Jose-Rafael <jdiaz5511@gmail.com>
2017-10-23 12:59:50 -0400

    added map tutorial

---
+++ MEAN_map/memap/public/js/app.js
@@ -0,0 +1,17 @@
+// Declares the initial angular module "meanMapApp". Module grabs other controllers and services.
+var app = angular.module('meanMapApp', ['addCtrl', 'geolocation', 'gservice', 'ngRoute']);
+// Configures Angular routing -- showing the relevant view and controller when needed.
+    .config(function($routeProvider){
+
+        // Join Team Control Panel
+        $routeProvider.when('/join', {
+            controller: 'addCtrl',
+            templateUrl: 'partials/addForm.html',
+
+            // Find Teammates Control Panel
+        }).when('/find', {
+            templateUrl: 'partials/queryForm.html',
+
+            // All else forward to the Join Team Control Panel
+        }).otherwise({redirectTo:'/join'})
+    });
